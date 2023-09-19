4ee53083e8329fcddba9b041a5700c26a9cc69ee
Rade Tomovic <rade.tomovic@outlook.com>
2023-09-19 22:35:34 +0200

    adding context accessor and correlation middleware for later use

---
+++ src/CompanyManager.Api/Configuration/CorrelationMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace interview_task_4create.Configuration;
+
+internal class CorrelationMiddleware
+{
+    internal const string CorrelationHeaderKey = "CorrelationId";
+
+    private readonly RequestDelegate _next;
+
+    public CorrelationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var correlationId = Guid.NewGuid();
+
+        context.Request.Headers.Add(CorrelationHeaderKey, correlationId.ToString());
+
+        await _next.Invoke(context);
+    }
+}
