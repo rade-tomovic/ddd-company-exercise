ba0dd3047ed51d1f9925a34011b5959ad773e18d
Rade Tomovic <rade.tomovic@outlook.com>
2023-09-24 22:07:38 +0200

    added CreatedAt field to employee entity

--- src/CompanyManager.Persistence/Domain/Employees/EmployeeDbEntity.cs
+++ src/CompanyManager.Persistence/Domain/Employees/EmployeeDbEntity.cs
@@ -7,5 +7,6 @@ public class EmployeeDbEntity
     public Guid Id { get; set; }
     public string Email { get; set; }
     public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
     public ICollection<CompanyEmployeeDbEntity> CompanyEmployees { get; set; }
 }
